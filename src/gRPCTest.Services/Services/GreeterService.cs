800214d0fe0c3099a70dd94377186c699a05e857
Bernardo Esbérard <bernardbr@gmail.com>
2021-05-01 17:57:26 -0300

    :tada: First commit.

---
+++ src/gRPCTest.Services/Services/GreeterService.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+using gRPCTest.Services.Protos;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace gRPCTest.Services.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation($"{nameof(SayHello)} started.");
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
---
+++ src/gRPCTest.Services/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Grpc.HealthCheck;
+using gRPCTest.Services.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace gRPCTest.Services
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHealthChecks("/healthcheck");
+                endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context
+                        .Response
+                        .WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddGrpc();
+
+            services
+                .AddHealthChecks();
+
+            services
+                .AddGrpcHttpApi();
+
+            services.AddSingleton<HealthServiceImpl>();
+
+            services
+                .AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                });
+
+            services
+                .AddGrpcSwagger();
+        }
+    }
+}
